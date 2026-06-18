8911cf57696462d2d0adeeba301d9d4a01a5c305
pedroteloeken <pedroteloeken2211@gmail.com>
2026-06-18 20:12:24 -0300

    Initial Commit

---
+++ EstacioneAqui/EstacioneAquiApp.swift
@@ -0,0 +1,17 @@
+//
+//  EstacioneAquiApp.swift
+//  EstacioneAqui
+//
+//  Created by Pedro Teloeken on 18/06/26.
+//
+
+import SwiftUI
+
+@main
+struct EstacioneAquiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
